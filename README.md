2efc5ffe1dd01b786fc9afc779e9115026281554
JonasKrau <jonaskrause1@gmail.com>
2023-12-30 20:08:22 +0100

    Clean Code and write Readme

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Python3-NmapScanner
-Simple network scanner - built with Python3 and Nmap.
+# Simple Nmap Automation Tool
+
+This Python script provides a simple interface for using Nmap to perform network scans. It allows users to select from different types of scans and to target specific IP addresses.
+
+## Features
+
+- SYN ACK Scan
+- UDP Scan
+- Comprehensive Scan
+
+## Requirements
+
+- Python 3
+- Nmap
+- python-nmap library
+
+## Installation
+
+Ensure you have Nmap installed on your system. Then, install the python-nmap library using pip:
+
+pip install python-nmap
+
+## Usage
+
+Run the script with Python 3:
+sudo python3 Scanner.py
+
+Follow the on-screen prompts to enter the IP address and select the type of scan you wish to perform.
+
+## Legal Notice
+
+This tool is a forked version intended for educational and legal purposes only. Do not use it for any illegal activities. Users are responsible for complying with all applicable laws and regulations regarding network scanning. Unauthorized scanning and/or breaking into computer networks is illegal and may result in legal actions.
+
+## Disclaimer
+
+The author of this fork is not responsible for any misuse or damage caused by this tool. Please use it responsibly.
+
+## License
+
+This project is licensed under the Apache License 2.0 - see the [LICENSE](LICENSE) file for details.
+
--- Scanner.py
+++ Scanner.py
@@ -2,35 +2,89 @@
 
 import nmap
 
-scanner = nmap.PortScanner()
+def create_scanner():
+    """
+    Creates and returns an Nmap PortScanner object.
+    """
+    return nmap.PortScanner()
 
-print("Welcome, this is a simple nmap automation tool")
-print("<----------------------------------------------------->")
+def get_ip_address():
+    """
+    Prompts the user to enter an IP address and returns it.
+    """
+    ip_addr = input("Please enter the IP address you want to scan: ")
+    print("The IP you entered is: ", ip_addr)
+    return ip_addr
 
-ip_addr = input("Please enter the IP address you want to scan: ")
-print("The IP you entered is: ", ip_addr)
-type(ip_addr)
+def get_scan_type():
+    """
+    Prompts the user to choose a type of scan and returns the user's choice.
+    Available scan types are:
+    1) SYN ACK Scan
+    2) UDP Scan
+    3) Comprehensive Scan
+    """
+    resp = input("""\nPlease enter the type of scan you want to run
+                    1)SYN ACK Scan
+                    2)UDP Scan
+                    3)Comprehensive Scan \n""")
+    print("You have selected option: ", resp)
+    return resp
 
-resp = input("""\nPlease enter the type of scan you want to run
-                1)SYN ACK Scan
-                2)UDP Scan
-                3)Comprehensive Scan \n""")
-print("You have selected option: ", resp)
-resp_dict={'1':['-v -sS','tcp'],'2':['-v -sU','udp'],'3':['-v -sS -sV -sC -A -O','tcp']}
-if resp not in resp_dict.keys():
-    print("enter a valid option")
-else:
-    print("nmap version: "sccaner.nmap_version())
-    scanner.scan(ip_addr,"1-1024",resp_dict[resp][0]) #the # are port range to scan, the last part is the scan type
-    print(scanner.scaninfo())
-    if scanner.scaninfo()=='up':
-        print("Scanner Status: ",scanner[ip_addr].state())
-        print(scanner[ip_addr].all_protocols())
-        print("Open Ports: ",scanner[ip_addr][resp_dict[resp][1]].keys())  #display all open ports
+def perform_scan(scanner, ip_addr, scan_type):
+    """
+    Performs the scan based on the user's chosen scan type on the specified IP address.
+    Returns the scanner object after the scan.
+    """
+    resp_dict = {
+        '1': ['-v -sS -Pn', 'tcp'],
+        '2': ['-v -sU -Pn', 'udp'],
+        '3': ['-v -sS -sV -sC -A -O -Pn', 'tcp']
+    }
 
+    if scan_type not in resp_dict.keys():
+        print("Enter a valid option")
+        return None
 
+    print("nmap version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, "1-1024", resp_dict[scan_type][0])
+    return scanner
 
+def display_results(scanner, ip_addr, scan_type):
+    """
+    Displays the results of the scan including the scanner status,
+    protocols used, and any open ports found.
+    """
+    resp_dict = {
+        '1': ['-v -sS -Pn', 'tcp'],
+        '2': ['-v -sU -Pn', 'udp'],
+        '3': ['-v -sS -sV -sC -A -O -Pn', 'tcp']
+    }
 
+    if 'up' in scanner[ip_addr].state():
+        print("\nScanner Status: ", scanner[ip_addr].state())
+        print("Protocols: ", scanner[ip_addr].all_protocols())
+        if resp_dict[scan_type][1] in scanner[ip_addr].all_protocols():
+            print("Open Ports: ", scanner[ip_addr][resp_dict[scan_type][1]].keys())
+        else:
+            print("No open ports found in specified protocol.")
+    else:
+        print("Host seems to be down.")
 
+def main():
+    """
+    Main function to run the Nmap automation tool.
+    """
+    print("Welcome, this is a simple nmap automation tool")
+    print("<----------------------------------------------------->")
 
+    scanner = create_scanner()
+    ip_addr = get_ip_address()
+    scan_type = get_scan_type()
+    scanner = perform_scan(scanner, ip_addr, scan_type)
 
+    if scanner:
+        display_results(scanner, ip_addr, scan_type)
+
+if __name__ == '__main__':
+    main()
